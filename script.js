5f1e16ca39a52c1af1b5a11bade5c7535cd227c2
Tssemi <127999956+tssemi@users.noreply.github.com>
2025-03-10 09:25:18 -0300

    Add a function to start the game. Create a factory function for players

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 function newGame() {
     let spots = document.querySelectorAll('.game-board .spot');
-    const  startGame = () => {
-        spots.forEach(e => {
-            e.textContent = '*';
-        });    
-    }
-    return {spots, startGame}
+    
+    const startGame = () => {
+        spots.forEach(e => e.textContent = '*');    
+    };
+
+    const check = () => {
+        
+    };
+    
+    return {spots, startGame, check}
+}
+
+function playerInfo(playerName) {
+    const name = playerName;
+    const getName = () => name;
+
+    let score = 0;
+    const getScore = () => score
+    const sumScore = () => ++score;
+
+    /*let sign;
+    const chooseSign = () => {
+        
+    };*/
+
+
+    return {getName, getScore, sumScore}
 }
 
 const gameBoard = newGame();
@@ -17,6 +38,7 @@ start.addEventListener('click', () => {
 
 gameBoard.spots.forEach(e => {
     e.addEventListener('click', () => {
-        e.textContent = 'X'
+        e.textContent = 'X';
+
     })
 });
