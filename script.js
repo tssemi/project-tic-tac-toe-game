71bad12676d5c8bd14c6894b9669310b2dd7da87
Tssemi <127999956+tssemi@users.noreply.github.com>
2025-10-22 09:41:48 -0300

    Create a function to iterate each cell

--- script.js
+++ script.js
@@ -4,11 +4,19 @@
 
 //Console game:
 //make every cell interactive for the user
-
-
-
-
+ function createGame() {
+    let cells = document.querySelectorAll(".cell");
 //every time the user clicks on a space an X sign must appear
+    
+    cells.forEach(e => {
+    e.addEventListener("click", () => {
+            e.textContent = "X";
+        })
+    });
+ }
+
+ const gameBoard = createGame();
+
 //immediately (for now maybe) after that an O sign must be shown in a random cell
 //whoever the user or the browser makes a line wins and the winner must be shown
 //to start the game again press the button
