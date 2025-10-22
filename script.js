4067231cfde3b313c83e2ed26133a88670a2b97c
Tssemi <127999956+tssemi@users.noreply.github.com>
2025-10-22 08:40:04 -0300

    Pseudo code

---
+++ script.js
@@ -0,0 +1,14 @@
+//Beginning:
+//when the start button is pressed the board must be clean
+//the user will have to put their name so it can be stored
+
+//Console game:
+//make every cell interactive for the user
+
+
+
+
+//every time the user clicks on a space an X sign must appear
+//immediately (for now maybe) after that an O sign must be shown in a random cell
+//whoever the user or the browser makes a line wins and the winner must be shown
+//to start the game again press the button
