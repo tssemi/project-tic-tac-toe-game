c328111866f0bf76be8f7c2754ef0e7a7ea3af64
Tssemi <127999956+tssemi@users.noreply.github.com>
2025-10-25 10:43:18 -0300

    Create a function to fill a random cell with O,to pretend to be the second player

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-//Beginning:
-//the user will have to put their name so it can be stored
-
 //Console game:
-//make every cell interactive for the user
  function createGame() {
     let cells = document.querySelectorAll('.cell');
-//every time the user clicks on a space an X sign must appear
-    
     cells.forEach(e => {
     e.addEventListener('click', () => {
+        if (e.textContent == ' ') {
             e.textContent = 'X';
+            browsersTurn();
+        };
+
         })
     });
-//when the start button is pressed the board must be clean
-    const bttnStart = document.querySelector('.bttn');
-    bttnStart.addEventListener('click', () => {
-        console.log('cells')
+//immediately (for now maybe) after that, an O sign must be shown in a random cell
+    const browsersTurn = () => {
+        let num = Math.floor(Math.random() * 8 + 1);
+        while (cells[num].textContent == 'X' || cells[num].textContent == 'O') {
+            num = Math.floor(Math.random() * 8) + 1;
+        }
+        cells[num].textContent = 'O';
+    }
+//make a function to check when the board is full or someone made a line
+
+
+    const startButton = document.querySelector('.button');
+    startButton.addEventListener('click', () => {
         cells.forEach(e => {
-            e.textContent = '';
+            e.textContent = ' ';
         });
     })
 
-
-    return {cells}
- }
-
- const gameBoard = createGame();
-
-//immediately (for now maybe) after that an O sign must be shown in a random cell
 //whoever the user or the browser makes a line wins and the winner must be shown
-//to start the game again press the button
+    return {cells}
+}
+
+const gameBoard = createGame();
