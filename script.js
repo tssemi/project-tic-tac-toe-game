fc94a5c29e6732904e73a8ec3a20d9029580f14d
Tssemi <127999956+tssemi@users.noreply.github.com>
2025-11-20 18:31:15 -0300

    Make the cells array global. Create a new factory function to store the gameboard

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-const gameBoard = {
-    player1: {
-        name: '',
-        sign: 'X'
-    },
-    player2: {
-        name: '',
-        sign: 'O'
-    },
-    start: startGame,
+const cells = document.querySelectorAll('.cell');
+const startButton = document.querySelector('.button');
+let gameBoard = createGame();
+
+startButton.addEventListener('click', () => {
+    gameBoard.players.p1 = prompt('X Player: ', );
+    gameBoard.players.p2 = prompt('O Player: ', );
+    gameBoard.game.finishingGame(gameBoard.game.startingGame(0, gameBoard.player1.name, gameBoard.player2.name));
+})
+
+function createGame() {
+    return {
+        players: {p1: '', p2: ''},
+
+
+        initiateGame() {
+            cells.forEach(e => e.textContent = ' ');
+        }
+
+
+    }
 
 }
 
-const startButton = document.querySelector('.button');
-    startButton.addEventListener('click', () => {
-        gameBoard.player1.name = prompt('Player X: ', );
-        gameBoard.player2.name = prompt('Player O: ', );
-        console.log(gameBoard.player1.name)
-        gameBoard.start(0);
-    })
-
 //Console game
- function startGame(num) {
-    let cells = document.querySelectorAll('.cell');
-    cells.forEach(e => {
-            e.textContent = ' ';
-        });
+ function startingGame(num, p1, p2) {
+    //const cells = document.querySelectorAll('.cell');
     let count = num;
 
-    cells.forEach(e => {
-    e.addEventListener('click', () => cellClicked(e))
-    });
+    cells.forEach(e => e.textContent = ' ');
+
+    cells.forEach(e => e.addEventListener('click', () => cellClicked(e)));
 //Player's input
     function cellClicked(e) {
-        if (count % 2 === 0) {
-            e.textContent = 'X';
-            count++;
-            if (checkBoard() == true) {
-                console.log('X won')
-            };
-            console.log(count)
-        } else {
-            e.textContent = 'O';
-            count++;
-            if (checkBoard() == true) {
-                console.log('O won')
-            };
-            console.log(count)
-        };
+        if (e.textContent == false) {
+            if (count % 2 === 0) {
+                e.textContent = 'X';
+                count++;
+                if (count >= 5) {
+                    if (checkBoard()[0] == true && checkBoard()[1] == 'X') gameOver(p1);
+                };
+            } else {
+                e.textContent = 'O';
+                count++;
+                if (count >= 5) {
+                    if (checkBoard()[0] == true && checkBoard()[1] == 'O') gameOver(p2);
+                };
+            };    
+        }
     }
 //Check board
     const checkBoard = () => {
+        let bool = false;
         const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
         [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
         for (let i = 0; i < lines.length; i++) {
@@ -61,19 +62,20 @@ const startButton = document.querySelector('.button');
             if (cellA == false || cellB == false || cellC == false) {
                 continue;
             } else if (cellA == cellB && cellB == cellC) {
-                gameOver();
-                return true
+                return [true, cellA];
+                
             };
         }
-        return false
+        return bool;
     };
 
-//Game Over
-    const gameOver = () => {
-        let cells = document.querySelectorAll('.cell');
-        cells.forEach(e => e.removeEventListener('click', () => cellClicked));
+    const gameOver = (winner) => {
+        return {winner}
     }
 
-    return {cells}
+    return {gameOver}
 }
 
+function finishingGame(winner) {
+    console.log('winner ' + winner);
+}
